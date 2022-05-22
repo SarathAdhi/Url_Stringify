1b4a41a0c02366a9871a09f3d5398a294e2afad2
SarathAdhi <s.sarathadhithya@gmail.com>
2022-05-22 12:50:52 +0530

    short url navigator fixed

---
+++ src/pages/id/[id].js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Router from 'next/router';
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import db from '../../backend/db';
+import { useEffect } from 'react';
+
+export async function getServerSideProps(context) {
+
+    const urlId = context.query.id;
+    const URL_Col = collection(db, 'urls');
+    const URL_Snapshot = await getDocs(URL_Col);
+    const neededURL = URL_Snapshot.docs.map(doc => doc.data()).filter((data) => {
+        return data.shortUrlCode === urlId;
+    }).map((data) => {
+        return data;
+    })
+
+    return {
+        props: {
+            data: neededURL
+        }
+    }
+}
+
+export default function Url({ data }) {
+    useEffect(() => {
+        async function routePath() {
+            if (data.length != 0)
+                await Router.push(data[0].fullUrl);
+            else
+                alert("The ID doesn't exist in the Database")
+        }
+        routePath();
+        console.log(data)
+    }, [])
+    return (
+        <div></div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,85 @@
-import { useState, useEffect } from 'react';
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import db from '../backend/db'
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import db from "../backend/db";
+import Link from "next/link";
 
 export default function Home() {
-  const [datasFromDB, setDatasFromDB] = useState([]);
-  const [fullUrl, setFullUrl] = useState('');
-  const [websiteUrl, setWebsiteUrl] = useState('');
-  const [minifiedUrl, setMinifiedUrl] = useState('');
+  const [fullUrl, setFullUrl] = useState("");
+  const [websiteUrl, setWebsiteUrl] = useState("");
+  const [minifiedUrl, setMinifiedUrl] = useState("");
 
-  const usersCollectionRef = collection(db, "urls")
-
-  async function getDatas() {
-    const URLCol = collection(db, 'urls');
-    const URLSnapshot = await getDocs(URLCol);
-    const URLList = URLSnapshot.docs.map(doc => doc.data());
-    setDatasFromDB(URLList);
-  }
+  const urlsCollectionRef = collection(db, "urls");
 
   useEffect(() => {
-    setWebsiteUrl(location.host)
-    getDatas();
-  }, [])
+    setWebsiteUrl(location.host);
+  }, []);
 
   async function createNewUrl() {
-    const uid = (new Date().getTime()).toString(36);
-    setMinifiedUrl(uid)
+    const uid = new Date().getTime().toString(36);
     const date = new Date();
-    await addDoc(usersCollectionRef, {
+    await addDoc(urlsCollectionRef, {
       fullUrl: fullUrl,
       shortUrlCode: uid,
-      date: `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
-    })
+      date: `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`,
+    });
+    setMinifiedUrl(uid);
   }
 
+  async function isThisUrlAlreadyExist() {
+    const URL_Snapshot = await getDocs(urlsCollectionRef);
+    const neededURL = URL_Snapshot.docs
+      .map((doc) => doc.data())
+      .filter((data) => {
+        return data.fullUrl === fullUrl;
+      })
+      .map((data) => {
+        return data;
+      });
 
+    if (neededURL.length === 0) createNewUrl();
+    else setMinifiedUrl(neededURL[0].shortUrlCode);
+  }
+
+  function isUrlValid() {
+    setMinifiedUrl('')
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" +
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+        "((\\d{1,3}\\.){3}\\d{1,3}))" +
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+        "(\\?[;&a-z\\d%_.~+=-]*)?" +
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    );
+
+    var res = !!pattern.test(fullUrl);
+    if (res) isThisUrlAlreadyExist();
+    else alert("Invalid URL");
+  }
 
   return (
-    <div className='h-screen bg-teal-300 flex justify-center'>
-      <div className='m-40'>
-        <input type="text" placeholder='Enter the URL' className='p-2 w-96 border-[1px] border-slate-800 rounded-lg focus:outline-none' onChange={(e) => setFullUrl(e.target.value)} />
-        <button className='p-2 ml-2 rounded-lg bg-green-500 text-black border-[1px] border-slate-800 duration-300 hover:bg-emerald-500' onClick={createNewUrl}>Submit</button>
-
-        {
-          minifiedUrl && (
-            <h2 className='mt-20 text-blue-600 underline text-center'>
-              <Link href={`/${minifiedUrl}`}>
-                <a>{websiteUrl + "/" + minifiedUrl}</a>
-              </Link>
-            </h2>
-          )
-        }
+    <div className="h-screen bg-teal-300 flex justify-center">
+      <div className="m-40">
+        <input
+          type="text"
+          placeholder="Enter the URL"
+          className="p-2 w-96 border-[1px] border-slate-800 rounded-lg focus:outline-none"
+          onChange={(e) => setFullUrl(e.target.value)}
+        />
+        <button
+          className="p-2 ml-2 rounded-lg bg-green-500 text-black border-[1px] border-slate-800 duration-300 hover:bg-emerald-500"
+          onClick={isUrlValid}
+        >
+          Submit
+        </button>
+        {minifiedUrl && (
+          <h2 className="mt-20 text-blue-600 underline text-center">
+            <Link href={`/id/${minifiedUrl}`}>
+              <a target="_blank">{websiteUrl + "/id/" + minifiedUrl}</a>
+            </Link>
+          </h2>
+        )}
       </div>
     </div>
-  )
+  );
 }
